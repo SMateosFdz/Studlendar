0717166e78653972652127bbeb4b3f3c000479b7
snty181 <sanmafer18@gmail.com>
2025-05-03 17:20:24 +0200

    feat: added new navigation functionality

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,27 +1,70 @@
 import { NavLink } from "@remix-run/react";
 
-function Navigation(){
-    return(
-        <nav id="full-navigation">
-            <ul className="navigation">
-                <li className="nav-item">
-                    <NavLink to={"/"} className={"link"}>Empezar pomodoro</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to={"/"} className={"link"}>Modo estudio</NavLink>
-                </li>
+function Navigation({currentPage}) {
+    let content;
+  switch(currentPage){
+    case "/":
+        content = <nav id="full-navigation">
+        <ul className="navigation">
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Empezar pomodoro
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Modo estudio
+            </NavLink>
+          </li>
 
-                <h1 id="nav-title">Studlendar</h1>
+          <h1 id="nav-title">Studlendar</h1>
 
-                <li className="nav-item">
-                    <NavLink to={"/"} className={"link"}>Apuntar horas de estudio</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to={"/"} className={"link"}>Configuración</NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Apuntar horas de estudio
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Configuración
+            </NavLink>
+          </li>
+        </ul>
+      </nav>;
+    break;
+
+    case "/proposals":
+        content = <nav id="full-navigation">
+        <ul className="navigation">
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Empezar pomodoro
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Modo estudio
+            </NavLink>
+          </li>
+
+          <h1 id="nav-title">Studlendar</h1>
+
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Apuntar horas de estudio
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to={"/"} className={"link"}>
+              Configuración
+            </NavLink>
+          </li>
+        </ul>
+      </nav>;
+    break;
+  }
+
+  return content;
 }
 
 export default Navigation;
