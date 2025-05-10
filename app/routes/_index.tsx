680db995640d277f45f0922016690e27bbd8e113
sanmate <santiago.mateos@estudiantes.uva.es>
2025-05-10 19:49:45 +0200

    Feat: added new functionality

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,28 +1,39 @@
-import type { ActionFunctionArgs} from "@remix-run/node";
+import type { ActionFunctionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { Form, Link, useActionData } from "@remix-run/react";
 import { getStoredUsers } from "~/data/users";
 import styles from "~/styles/index.css";
+import logo from "~/images/Studlendar.png";
 
 export default function Index() {
   const data: any = useActionData();
-  
+
   return (
     <>
-      <h1>Bienvenido a Studlendar</h1>
-      <h2>Inicio de sesión</h2>
-      <Form method="post" id="sessionForm">
-        <label htmlFor="name">Nombre:</label>
-        <input type="text" name="name" required></input>
-        <label htmlFor="password">Contraseña:</label>
-        <input type="password" name="password" required></input>
-        <button>Iniciar sesión</button>
-      </Form>
+      <header>
+        <h1>Bienvenido a Studlendar</h1>
+      </header>
 
-      {data?.message && <p>{data.message}</p>}
+      <main>
+        <h2>Una aplicación de calendario para estudiantes que quieran mejorar su organización</h2>
+        <img src={logo} alt=""></img>
+        <h3>Inicio de sesión</h3>
+        <Form method="post" id="sessionForm">
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" name="name" id="name" required></input>
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" name="password" id="password" required></input>
+          <button>Iniciar sesión</button>
+        </Form>
+        {data?.message && <div className="error">{data.message}</div>}
+        
+      </main>
 
-      <h3>¿No tienes cuenta?</h3>
-      <Link to="/newAccount">Crea una nueva cuenta</Link>
+      <footer>
+        <h3>¿No tienes cuenta?</h3>
+        <Link to="/newAccount">Crea una nueva cuenta</Link>
+      </footer>
+      
     </>
   );
 }
@@ -32,21 +43,18 @@ export async function action({ request }: ActionFunctionArgs) {
   const existingUsers = await getStoredUsers();
   const userData = Object.fromEntries(formData);
 
-
-  for(var nameComprobation of existingUsers){
-    if(userData.name.toString() === nameComprobation.name){
-      if(userData.password.toString() !== nameComprobation.password){
-        return { message: "Wrong password, try again" };
+  for (var nameComprobation of existingUsers) {
+    if (userData.name.toString() === nameComprobation.name) {
+      if (userData.password.toString() !== nameComprobation.password) {
+        return { message: "Contraseña incorrecta, prueba de nuevo" };
+      } else{
+          return redirect("/main");
       }
     }
   }
-
-  return redirect("/main"); // redirection of the user after saving the data, it's a good practice to have this return statement
+  return { message: "Nombre de usuario incorrecto, prueba de nuevo"};
 }
 
 export function links() {
-  return [
-    { rel: "stylesheet", href: styles },
-  ];
+  return [{ rel: "stylesheet", href: styles }];
 }
-
