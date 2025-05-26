57b59c34150f0bedfc699a08c22a291f06acf96f
sanmate <santiago.mateos@estudiantes.uva.es>
2025-05-26 23:41:48 +0200

    Feat: added events functionality to save in db

---
+++ app/data/events.server.js
@@ -0,0 +1,23 @@
+import { prisma } from './database.server';
+
+export async function addEvent(eventData) {
+    try {
+        await prisma.event.create({data: {
+
+            /* blockId : eventData.blockId, */
+            name: eventData.name,
+            color: eventData.color,
+            date: new Date(eventData.date),
+            notes: eventData.notes,
+            subject: {
+                connect: {name: eventData.subjectName},
+            },
+            
+        }});
+
+        return {ok: true};
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+}
