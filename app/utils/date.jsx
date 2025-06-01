5e7c970b43c6e6d2d79f52ab90f067d4ed4d9ec9
sanmate <santiago.mateos@estudiantes.uva.es>
2025-06-01 16:03:18 +0200

    Feat: added new functionality to get date values

--- app/utils/date.jsx
+++ app/utils/date.jsx
@@ -7,3 +7,17 @@ export function getCurrentDate() {
   const dayOfWeek = currentDate.getDay();
   return { month, hours, minutes, seconds, dayOfWeek};
 }
+
+export function getDateValues(date){
+  const currentDate = new Date(date);
+  const month = currentDate.getMonth() + 1;
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+  let dayOfWeek = currentDate.getDay();
+  const year = currentDate.getFullYear();
+  if(dayOfWeek == 0){
+    dayOfWeek = 7;
+  }
+  return { year, month, hours, minutes, seconds, dayOfWeek};
+}
