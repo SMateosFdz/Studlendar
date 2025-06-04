010e44966bf1f492a1a7372b64c605775ae84c8a
sanmate <santiagomateosfernandez@gmail.com>
2025-06-04 15:32:50 +0200

    Fix: fixed interfaces adding date property

--- app/interfaces/classblock.tsx
+++ app/interfaces/classblock.tsx
@@ -4,4 +4,5 @@ export interface ClassBlock {
   subjectName: string;
   repetition: string;
   time: string;
+  date: string;
 }
--- app/interfaces/studyblock.tsx
+++ app/interfaces/studyblock.tsx
@@ -4,4 +4,5 @@ export interface StudyBlock {
   subjectName: string;
   time: string;
   repetition: string;
+  date: string;
 }
