eda7dbce7f170d79060d9030ed628d7de5b05242
sanmate <santiagomateosfernandez@gmail.com>
2025-06-23 01:46:31 +0200

    Feat: added new packages for new functionality and config for the packages.

--- remix.config.js
+++ remix.config.js
@@ -2,4 +2,26 @@
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverModuleFormat: "cjs",
+  browserNodeBuiltinsPolyfill: {
+    modules: {
+      events: true, 
+      url: true,
+      util: true,
+      fs: true,
+      http: true,
+      https: true,
+      zlib: true,
+      stream: true,
+      net: true,
+      dns: true,
+      os: true,
+      path: true,
+      crypto: true,
+      tls: true,
+      child_process: true,
+      timers: true,
+      process: true,
+      "fs/promises": true,
+      "timers/promises": true,
+  }}
 };
