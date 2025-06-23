797c2b9ad0d44d6e2563dfc479be198f4c626b60
sanmate <santiagomateosfernandez@gmail.com>
2025-06-23 19:18:18 +0200

    Fix: deleted unused code

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -3,57 +3,6 @@ import { NavLink } from "@remix-run/react";
 function Navigation({ currentPage }) {
   let content;
   switch (currentPage) {
-    case "/pomodoro":
-      content = (
-        <header>
-          <h1 id="title">Studlendar</h1>
-          <nav id="full-navigation">
-          <ul className="navigation">
-            <li className="nav-item">
-              <NavLink to={"/main"} className={"link"}>
-                Volver al calendario
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        </header>
-      );
-      break;
-    
-    case "/configuration":
-      content = (
-        <header>
-          <h1 id="title">Studlendar</h1>
-          <nav id="full-navigation">
-          <ul className="navigation">
-            <li className="nav-item">
-              <NavLink to={"/main"} className={"link"}>
-                Volver al calendario
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        </header>
-      );
-      break;
-    
-    case "/study-mode":
-      content = (
-        <header>
-          <h1 id="title">Studlendar</h1>
-          <nav id="full-navigation">
-          <ul className="navigation">
-            <li className="nav-item">
-              <NavLink to={"/main"} className={"link"}>
-                Volver al calendario
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        </header>
-      );
-      break;
-
       case "/importFile":
       content = (
         <header>
@@ -67,7 +16,7 @@ function Navigation({ currentPage }) {
             </li>
           </ul>
           </nav>
-          <h2 id="subtitle">Importar fichero ICS y extraer eventos</h2>
+          <h2 id="subtitle">Importar fichero ICS y extraer bloques y eventos</h2>
         </header>
       );
       break;
