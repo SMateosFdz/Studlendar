fda5df1a45106b0e70c195c3881fe62131f8f7fa
sanmate <santiagomateosfernandez@gmail.com>
2025-06-23 18:16:18 +0200

    Feat: added new functionality for the creation of the proposals

--- app/routes/formOne.tsx
+++ app/routes/formOne.tsx
@@ -1,7 +1,9 @@
 import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 import { Form, redirect } from "@remix-run/react";
-import { userId } from "~/cookies.server";
+import { useState } from "react";
 import { prisma } from "~/data/database.server";
+import { updateUser } from "~/data/users.server";
+import { getSession } from "~/sessions.server";
 import styles from "~/styles/formOne.css";
 
 export const meta: MetaFunction = () => {
@@ -11,33 +13,83 @@ export const meta: MetaFunction = () => {
 };
 
 export default function FormOne() {
+  const daysWeek: { id: string; checked: boolean }[] = [];
+  const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+
+  const createInitialDays = () => {
+    days.map((day) => {
+      daysWeek.push({
+        id: day,
+        checked: false,
+      });
+    })
+
+    return daysWeek;
+  };
+
+  const [checkedDays, setCheckedDays] = useState(createInitialDays);
+
+  const handleCheckboxChange = (id: string) => {
+    setCheckedDays((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
 
   return (
     <>
       <h1>Bienvenido a Studlendar</h1>
       <h2>Formulario inicial</h2>
+      <p>
+        Sí estás aquí, es porque estás por primera vez en esta aplicación. <br/>
+        Es importante que realices un cuestionario inicial antes de empezar a usar la aplicación, merece la pena. <br/>
+        Con éste podrás configura la aplicación a tu gusto (siempre podrás reconfigurarla más adelante).
+      </p>
       <div id="sessionForm">
         <Form method="post">
-          <label htmlFor="momento" id="momento">¿En qué momento del día prefieres los bloques de estudio?</label>
-          <fieldset id="momento">
-            <input type="checkbox" id="mañana"></input>
-            <label htmlFor="mañana">Mañana</label>
-            <input type="checkbox" id="tarde"></input>
-            <label htmlFor="tarde">Tarde</label>
-            <input type="checkbox" id="noche"></input>
-            <label htmlFor="noche">Noche</label>
+          <label>Elige los días en los que quieres bloques de estudio: </label>
+          <fieldset className="days-btn-container">
+            <input className="day-btn" id="monday" type="checkbox" onChange={() => handleCheckboxChange("monday")} />
+            <label className="day-label" htmlFor="monday">L</label>
+
+            <input className="day-btn" id="tuesday" type="checkbox" onChange={() => handleCheckboxChange("tuesday")} />
+            <label className="day-label" htmlFor="tuesday">M</label>
+
+            <input className="day-btn" id="wednesday" type="checkbox" onChange={() => handleCheckboxChange("wednesday")} />
+            <label className="day-label" htmlFor="wednesday">X</label>
+
+            <input className="day-btn" id="thursday" type="checkbox" onChange={() => handleCheckboxChange("thursday")} />
+            <label className="day-label" htmlFor="thursday">J</label>
+
+            <input className="day-btn" id="friday" type="checkbox" onChange={() => handleCheckboxChange("friday")} />
+            <label className="day-label" htmlFor="friday">V</label>
+
+            <input className="day-btn" id="saturday" type="checkbox" onChange={() => handleCheckboxChange("saturday")} />
+            <label className="day-label" htmlFor="saturday">S</label>
+
+            <input className="day-btn" id="sunday" type="checkbox" onChange={() => handleCheckboxChange("sunday")} />
+            <label className="day-label" htmlFor="sunday">D</label>
           </fieldset>
           <hr></hr>
-          <label htmlFor="pomodoro">
-            ¿Quieres complementar los bloques con el método Pomodoro?
-          </label>
-          <fieldset id="pomodoro">
-            <input type="radio" required value="si" name="pomodoro"></input>
-            <label>Sí</label>
-            <input type="radio" required value="no" name="pomodoro"></input>
-            <label>No</label>
-          </fieldset>
-          <hr></hr>
+          {checkedDays.map((day) => {
+            if (day.checked) {
+              return (
+                <>
+                  <label htmlFor={`momento-${day.id}`} id={`momento-${day.id}`}>¿En qué momento del día prefieres los bloques de estudio para el {day.id.id}?</label>
+                  <fieldset id={`momento-${day.id}`}>
+                    <input type="checkbox" id={`mañana-${day.id}`} name={`mañana-${day.id}`}></input>
+                    <label htmlFor={`mañana-${day.id}`}>Mañana</label>
+                    <input type="checkbox" id={`tarde-${day.id}`} name={`tarde-${day.id}`}></input>
+                    <label htmlFor={`tarde-${day.id}`}>Tarde</label>
+                    <input type="checkbox" id={`noche-${day.id}`} name={`noche-${day.id}`}></input>
+                    <label htmlFor={`noche-${day.id}`}>Noche</label>
+                  </fieldset>
+                  <hr></hr>
+                </>
+              )
+            }
+          })}
           <input
             type="submit"
             name="move"
@@ -52,15 +104,32 @@ export default function FormOne() {
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const intent = formData.get("move");
-  const cookie = await userId.parse(request.headers.get("Cookie"));
-  const existingSubjects = await prisma.subject.findMany({
-    where: { authorId: cookie.userId },
+  let preferences = {
+    "monday": [],
+    "tuesday": [],
+    "wednesday": [],
+    "thursday": [],
+    "friday": [],
+    "saturday": [],
+    "sunday": [],
+  };
+
+  formData.forEach((element, key) => {
+    if(key.includes("-")){
+      const [value, day] = key.split("-");
+      preferences[day].push(value); 
+    }
   });
 
+  const session = await getSession(request);
+  const user = await prisma.user.findUnique({
+    where: { nameUser: session.data.userId },
+  });
+
+  updateUser(user, preferences);
+
   if (intent === "Guardar e ir al siguiente paso") {
-    if (existingSubjects.length !== 0) {
-      return redirect("/configurationForm");
-    }
+    return redirect("/configurationForm");
   }
 
   throw new Error("Acción desconocida");
