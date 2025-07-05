1c76dc0b7366ebf29e4e38b15a72d404bb786b55
sanmate <santiagomateosfernandez@gmail.com>
2025-07-05 22:40:05 +0200

    Feat: added new functionality to the creation of proposals

--- app/routes/proposals.tsx
+++ app/routes/proposals.tsx
@@ -9,26 +9,63 @@ import { addStudyBlock } from "~/data/studyBlocks.server";
 import { getDaysOfWeek } from "~/utils/date";
 import type { StudyBlock } from "~/interfaces/studyblock";
 
-function showSubjectProposals(subject: any, preferences) {
+function getSubjectProposals(subject: any, preferences, allProposals) {
   const matrix = Array.from({ length: 200 }, (_, i) => i);
-  const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+  const days = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
   const cols = 8;
   const rows = 25;
-  const result = [];
+  const result: number[] = [];
+  let lastBlock, preference, day, number;
+  let prefs: number[] = [];
 
-  let preference, day, number;
+  const blocks = Math.floor(Number(subject.hours) / Number(subject.sessionSize));
 
-  for (let i = 0; i < subject.hours; i++) {
+  const pendingTime = Number(subject.hours) - blocks * Number(subject.sessionSize);
+
+  pendingTime !== 0 ? (lastBlock = pendingTime) : lastBlock = 0;
+
+  Object.values(preferences).map((pref, index) => {
+    if (pref.length > 0) {
+      prefs.push(index);
+    }
+  });
+
+  const realHours = [];
+
+  for (let i = 0; i < blocks; i++) {
+    realHours.push(Number(subject.sessionSize));
+  }
+
+  lastBlock != 0 ? realHours.push(lastBlock) : realHours.push();
+  let j = 0;
+
+  for (let i = 0; i < realHours.length; i++) {
+    let proposal = 0;
+    let flag = true;
     do {
-      number = Math.floor(Math.random() * Object.keys(preferences).length);
-      day = days[number];
-      preference = preferences[day];
-    } while (preference.length == 0);
-    let proposal = getRandomValueFromMatrix(matrix, cols, rows, day, preference, number + 1);
-    do {
-      proposal = getRandomValueFromMatrix(matrix, cols, rows, day, preference, number + 1);
-    } while (result.includes(proposal));
-    result.push(proposal);
+      flag = true;
+      do {
+        day = days[j];
+        preference = preferences[day];
+        j != 6 ? j++ : j = 0;
+      } while (preference.length == 0);
+      proposal = getRandomValueFromMatrix(matrix, cols, preference, j + 1, realHours[i]);
+  
+      // eslint-disable-next-line no-loop-func
+      allProposals.map((prop: number[]) => {
+        for (let i = 0; i < prop[1]; i++) {
+          for(let x = 0; x < realHours[i]; x++){
+            if (proposal + x * 8 == prop[0] + i * 8) {
+              flag = false;
+            }
+          }
+        }
+      })
+
+
+    } while (!flag);
+    allProposals.push([proposal, realHours[i]]);
+    result.push([proposal, realHours[i]]);
   }
 
   const proposal: Proposal = {
@@ -37,11 +74,12 @@ function showSubjectProposals(subject: any, preferences) {
     proposals: result,
   };
 
-  return proposal;
+  console.log(proposal);
+
+  return { proposal, allProposals };
 }
 
-function getRandomValueFromMatrix(matrix, cols, rows, day, preference, col) {
-  //const col = Math.floor(Math.random() * (cols - 1)) + 2; // 2-8
+function getRandomValueFromMatrix(matrix, cols, preference, col, realHours) {
 
   const rowRanges = {
     "mañana": { start: 9, end: 15 },
@@ -53,7 +91,11 @@ function getRandomValueFromMatrix(matrix, cols, rows, day, preference, col) {
 
   const { start, end } = rowRanges[selectedRange];
 
-  const row = Math.floor(Math.random() * (end - start + 1)) + start;
+  let row = Math.floor(Math.random() * (end - start + 1)) + start;
+
+  if (row + realHours - 1 > 24) {
+    row = row - (realHours - 1);
+  }
 
   const index = (row - 1) * cols + col;
 
@@ -74,13 +116,15 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const preferences = user?.preferences;
 
   let selectedValues: Proposal[] = [];
+  let array1 = [];
 
   const values = existingSubjects.map(async (subject) => {
     const studyBlocks = await prisma.studyBlock.findMany({
       where: { subjectId: subject.id },
     });
     if (studyBlocks.length == 0) {
-      const proposal = showSubjectProposals(subject, preferences);
+      const { proposal, allProposals } = getSubjectProposals(subject, preferences, array1);
+      array1.push(allProposals);
       return proposal;
     }
   });
@@ -99,7 +143,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 interface Proposal {
   id: string;
   name: string;
-  proposals: number[];
+  proposals: any;
 }
 
 export default function Proposals() {
@@ -124,9 +168,6 @@ export default function Proposals() {
 
   const [isVisible, setIsVisible] = useState(true);
   const [isChecked, setIsChecked] = useState(createInitialSubjects);
-  const [numberChecked, setNumberChecked] = useState(isChecked.length);
-
-  console.log(selectedValues);
 
   const toggleVisibility = () => {
     setIsVisible((prev) => !prev);
@@ -164,14 +205,15 @@ export default function Proposals() {
         <h1 id="title">Studlendar</h1>
       </header>
       <main>
-        <button
+        {/* <button
           className={`proposals-button visible--${!isVisible}`}
           onClick={toggleVisibility}
         >
           Abrir listado de propuestas
-        </button>
+        </button> */}
 
-        <div className="calendar">
+        <div className="container">
+          <div className="calendar">
           <div className="calendar__corner" key={0}></div>
           {daysOfWeek.map((day, index) => (
             <div className={`calendar__day ${index + 1 == 7 ? "final-day" : ""}`} key={index + 1}>
@@ -189,8 +231,8 @@ export default function Proposals() {
                 key={index + 24}
               >
                 {selectedValues.map((value: any) =>
-                  value.proposals.map((proposal: number, ind: number) =>
-                    proposal === index + 24
+                  value.proposals.map((proposal: number[], ind: number) =>
+                    proposal[0] === index + 24
                       ? isChecked.map((obj) =>
                         obj.id == value.name + ind ? (
                           obj.checked === true ?
@@ -199,6 +241,7 @@ export default function Proposals() {
                                 className="class-item"
                                 style={{
                                   top: `calc(3dvh + 3.4dvh * ${value.name})`,
+                                  height: `calc(${proposal[1]} * 3.4dvh)`,
                                   left: `calc(13dvw * (${value.name - value.name % 8 - 1}) + 3dvw)`,
                                   position: "absolute",
                                 }}
@@ -221,14 +264,77 @@ export default function Proposals() {
           )}
         </div>
 
-        <div className={`popup-visible--${isVisible}`}>
+        <div className="proposals">
+          <h2>Propuesta de bloques de estudio</h2>
+            <p>Ahora se te ofrecen una serie de propuesta de bloques de estudio respecto a las asignaturas creadas
+              (o que actualmente no tienen bloques de estudio). Selecciona los bloques de estudio que prefieras:</p>
+            <Form method="post">
+              {selectedValues.map(
+                (
+                  subject: { id: string; name: string; proposals: number[] },
+                  index: number,
+                ) => {
+                  const checkedCount = subject.proposals.reduce((count, _, idx) => {
+                    return isChecked.find((obj) => obj.id === subject.name + idx)?.checked
+                      ? count + 1 : count;
+                  }, 0);
+
+                  return (
+                    <>
+                      <input type="hidden" id={"subjectId"} name={"subjectId"} value={subject.id}></input>
+                      <label htmlFor={subject.name} className={`${checkedCount === 0 ? "proposal--red" : ""}`}>
+                        {`${subject.name}: 
+                      ${checkedCount}  
+                      bloque${checkedCount == 1 ? "" : "s"} 
+                      seleccionado${checkedCount == 1 ? "" : "s"} de ${subject.proposals.length}`}</label>
+                      {subject.proposals.map((element, index) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <input
+                          id={"" + index}
+                          type="checkbox"
+                          value={element}
+                          name={subject.name + index}
+                          checked={
+                            isChecked.find((obj) => obj.id === subject.name + index)
+                              ?.checked
+                          }
+                          onChange={() =>
+                            handleCheckboxChange(subject.name + index)
+                          }
+                        ></input>
+                      ))}
+                      <hr></hr>
+                    </>)
+                }
+              )}
+              <input
+                type="button"
+                value="Seleccionar todos"
+                onClick={handleAllCheck}
+              ></input>
+              <br></br>
+              <input
+                type="submit"
+                name="close"
+                value="Guardar y volver a configuración"
+              ></input>
+              <input
+                type="submit"
+                name="close"
+                value="Guardar e ir al calendario"
+              ></input>
+            </Form>
+        </div>
+        </div>
+        
+        {/* <div className={`popup-visible--${isVisible}`}>
           <div className={`proposals-visible--${isVisible}`}>
             <span className="close" id="closePopup" onClick={toggleVisibility}>
               &times;
             </span>
-            <h2>Listado de propuestas</h2>
-            <p>En esta interfaz se te ofrecen una serie de propuestas respecto a las asignaturas creadas
-              (o que actualmente no tienen bloques de estudio):</p>
+            <h2>Propuesta de bloques de estudio</h2>
+            <p>Ahora se te ofrecen una serie de propuesta de bloques de estudio respecto a las asignaturas creadas
+              (o que actualmente no tienen bloques de estudio). Selecciona los bloques de estudio que prefieras:</p>
             <Form method="post">
               {selectedValues.map(
                 (
@@ -286,7 +392,7 @@ export default function Proposals() {
               ></input>
             </Form>
           </div>
-        </div>
+        </div> */}
       </main>
     </>
   );
@@ -303,21 +409,19 @@ export async function action({ request }: ActionFunctionArgs) {
       if (key === "subjectId") {
         subjectId = element.toString();
       } else {
-        const dayIndex = Number(element.toString()) % 8;
-        const startHour = (Number(element.toString()) - dayIndex - 24) / 8 + 2;
+        const dayIndex = Number(element.toString().split(",")[0]) % 8;
+        const startHour = (Number(element.toString().split(",")[0]) - dayIndex - 24) / 8 + 2;
         const year = new Date().getFullYear();
         const month = new Date().getMonth();
         const days = getDaysOfWeek(new Date());
-
-        const date = new Date(year, month, days[dayIndex - 1], startHour);
+        const date = new Date(year, month, days[0][dayIndex - 1], startHour);
 
         elements.push({
-          id: key.split(/(\d+)/)[0] + date.toISOString(),
+          id: key.split(/(\d+)/)[0] + date.toISOString().replace(".000", ""),
           name: key.toString(),
           subjectId: subjectId,
-          time: "1",
-          repetition: "semanal",
-          blockId: element.toString(),
+          time: element.toString().split(",")[1],
+          blockId: element.toString().split(",")[0],
           subjectName: key.split(/(\d+)/)[0],
           date: date.toISOString().replace(".000", ""),
           notes: "",
@@ -325,8 +429,9 @@ export async function action({ request }: ActionFunctionArgs) {
         });
       }
     }
-
   });
+
+
 
   elements.forEach((element) => {
     addStudyBlock(element);
